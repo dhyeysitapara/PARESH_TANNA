0adb91cb6feb9b3dac7480a8f5c6477950df6c61
Dhyey <dsitapara@gmail.com>
2025-07-03 10:44:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PARESH_TANNA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //NumberFormatInfo OF ROW
+            for(int i =0;i < 5; i++)
+            {
+                //NumberFormatInfo of COLUMNS
+                for(int j =0 ;j <= i; j++)
+                {
+                    Console.Write("*");
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
