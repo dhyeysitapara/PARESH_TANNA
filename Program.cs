d5b71ea70bb631eee622af6267460caf24ca0d0b
Dhyey <dsitapara@gmail.com>
2025-07-03 11:25:29 +0530

    pattern 6

--- Program.cs
+++ Program.cs
@@ -15,9 +15,13 @@ namespace PARESH_TANNA
             for(int i =1 ;i <=5; i++)
             {
                 //NumberFormatInfo of COLUMNS
-                for(int j = 1;j <= i; j++)
+                for(int j =1; j<= 5-i; j++)
                 {
-                    Console.Write(j);
+                    Console.Write(" ");
+                }
+                    for( int j = 1; j <= i; j++) 
+                {
+                    Console.Write("*");
                 }
                 Console.WriteLine();
             }
